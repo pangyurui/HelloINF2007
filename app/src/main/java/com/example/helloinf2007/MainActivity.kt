5ea53e4d4d9e379e50f3a441ce43cfdf3a5f665f
yu rui <rui2gen@gmail.com>
2024-01-18 16:54:04 +0800

    changed name to Yu Rui

--- app/src/main/java/com/example/helloinf2007/MainActivity.kt
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.helloinf2007
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +19,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
